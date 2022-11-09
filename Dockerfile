0b50661ed6245adaaf7187b336142e65817ac781
Oleg <Yapparov>
2022-11-09 12:35:27 +0100

    Updated contact photo, styling

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.9-slim
+WORKDIR /app
+
+ENV VIRTUAL_ENV=/opt/venv
+RUN python -m venv $VIRTUAL_ENV
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+
+# Install build software
+RUN apt update
+RUN apt -y upgrade
+RUN apt install -y zip
+RUN apt install -y curl
+RUN curl -fsSL https://deb.nodesource.com/setup_16.x | bash - && apt install -y nodejs
+RUN corepack enable
+RUN npm install -g @angular/cli
+
+# Install dependencies
+RUN python -m pip install --upgrade pip
+COPY ./requirements.txt ./requirements.txt
+RUN pip install -r ./requirements.txt
+
+CMD [ "./build.sh" ]
--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+rm --force vcelnice.zip &&
 (
-  cd client && npm run build && (
+  cd client && npm install --force && npm run build && (
     cd dist &&
       sed -i 's+styles.css+/assets/styles.css+g' index.html &&
       sed -i 's+runtime.js+/assets/runtime.js+g' index.html &&
@@ -9,30 +10,39 @@
       sed -i 's+scripts.js+/assets/scripts.js+g' index.html &&
       sed -i 's+main.js+/assets/main.js+g' index.html
   )
-) && zip -r \
-  --exclude=*node_modules* \
-  --exclude=*.angular* \
-  --exclude=*client/e2e* \
-  --exclude=*client/src* \
-  --exclude=*client/.editorconfig* \
-  --exclude=*client/angular.json* \
-  --exclude=*client/karma.conf.js* \
-  --exclude=*client/package.json* \
-  --exclude=*client/package-lock.json* \
-  --exclude=*client/protractor.conf.js* \
-  --exclude=*client/proxy.conf.json* \
-  --exclude=*client/README.md* \
-  --exclude=*client/tsconfig.json* \
-  --exclude=*client/tslint.json* \
-  --exclude=*.venv* \
-  --exclude=*.git* \
-  --exclude=*.idea* \
-  --exclude=*__pycache__* \
-  --exclude=*build.sh* \
-  --exclude=*db.sqlite3* \
-  --exclude=*vcelnice/logs* \
-  --exclude=*vcelnice/media* \
-  --exclude=*vcelnice/static_root* \
-  --exclude=*resources.txt* \
-  --exclude=*README.md* \
-  build.zip .
+) && python manage.py collectstatic --noinput \
+  && zip -r \
+    --exclude=*client/.angular* \
+    --exclude=*client/dist* \
+    --exclude=*client/e2e* \
+    --exclude=*client/node_modules* \
+    --exclude=*client/src* \
+    --exclude=*client/.editorconfig* \
+    --exclude=*client/angular.json* \
+    --exclude=*client/karma.conf.js* \
+    --exclude=*client/package.json* \
+    --exclude=*client/package-lock.json* \
+    --exclude=*client/protractor.conf.js* \
+    --exclude=*client/proxy.conf.json* \
+    --exclude=*client/README.md* \
+    --exclude=*client/tsconfig.json* \
+    --exclude=*client/tslint.json* \
+    --exclude=*tests* \
+    --exclude=*vcelnice/logs* \
+    --exclude=*vcelnice/media* \
+    --exclude=*.venv* \
+    --exclude=*.git* \
+    --exclude=*.idea* \
+    --exclude=*__pycache__* \
+    --exclude=*db.sqlite3* \
+    --exclude=*.env.tmpl* \
+    --exclude=*.gitignore* \
+    --exclude=*app.py* \
+    --exclude=*build.sh* \
+    --exclude=*builder.yml* \
+    --exclude=*Dockerfile* \
+    --exclude=*manage.py* \
+    --exclude=*README.md* \
+    --exclude=*requirements.txt* \
+    --exclude=*resources.txt* \
+    vcelnice.zip .
